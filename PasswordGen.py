1f681f6f2f61781024563c5caf17217da58b071b
MLAJILI27 <157696031+MLAJILI27@users.noreply.github.com>
2024-01-25 12:38:23 +0100

    Pswd_Generator
    Generate a strong password with Python

---
+++ PasswordGen.py
@@ -0,0 +1,9 @@
+import string
+import random
+import string
+characters = string.ascii_letters + string.punctuation  + string.digits
+a= ''.join(random.choice(characters)
+
+for i in range(random.randint(0,8),16))
+
+print(a)
