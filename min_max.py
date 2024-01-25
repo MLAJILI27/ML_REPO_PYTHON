ce777d03311736ab7ec45e5dd65226e1973c2e73
MLAJILI27 <157696031+MLAJILI27@users.noreply.github.com>
2024-01-25 12:32:46 +0100

    Min_Max
    Display Min and Max from given numbers

---
+++ min_max.py
@@ -0,0 +1,9 @@
+"""
+Écrire un programme min_max.py, qui demande de saisir 2 valeurs et qui affiche la plus petite des 2 valeurs
+"""
+x= input('Please enter a number: ')
+y= input('Please enter a number: ')
+if x<y:
+    print('la petite valeur est ',x)
+else:
+    print('la petite valeur est ',y)
