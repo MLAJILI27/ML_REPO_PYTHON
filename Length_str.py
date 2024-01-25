c5c8d809d453d30a00d80426a743f3c45230b06d
MLAJILI27 <157696031+MLAJILI27@users.noreply.github.com>
2024-01-25 12:11:19 +0100

    Length_str Function
    Display longest chain of characters between 2

---
+++ Length_str.py
@@ -0,0 +1,16 @@
+'''
+Écrire un script longeur_chaine.py, qui demande de saisir 2 chaînes de caractères et qui affiche la plus grande des 2 chaînes
+(celle qui a le plus de caractères).
+'''
+'''
+a= input('please enter a string: ')
+b= input('please enter a string: ')
+if len(a)< len(b):
+    print(b,'contain more caracters')
+elif len(a)==len(b):
+    print('equal length')
+else:
+    print(a,'contain more caracters')
+'''
+for i in range(0,16):
+    print(i)
