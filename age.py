6cb397121dcd91f7a0fc4906fae00fa2385a0cc4
MLAJILI27 <157696031+MLAJILI27@users.noreply.github.com>
2024-02-21 10:17:31 +0100

    Update age.py

--- age.py
+++ age.py
@@ -1,7 +1,7 @@
 name = input("please enter your name: ")
 age = int(input("please enter your age: "))
 
-year = str((2020 - age) + 100)
+year = str((2024 - age) + 100)
 
 print(name+" will be 100 years in "+year)
 
